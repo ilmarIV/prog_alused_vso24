741784229af3bf2d0b5b2c16125854c74eeb5b77
VI <ilmar.vodi@voco.ee>
2024-10-03 10:25:04 +0300

    2yl v6rdluse muutus

--- topic_3/yl3.2.js
+++ topic_3/yl3.2.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 rl.question('sisestage ringide arv: ', laps => {
     let carrots = 0
     for (let i = 1; i <= laps; i++) {
-        if (i % 2 == 0){
+        if (i % 2 === 0){
             carrots += i
         }
     }
